704b1580a2c6d64ad882eeb4458c97cac5bf17b1
CodeMS <75904467+brunnobenites@users.noreply.github.com>
2025-02-27 15:37:48 -0400

    Adiciona .env pro frontend com REACT_APP_API_URL

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,8 @@
+// src/axios.js
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/', // URL base do seu backend FastAPI
+  baseURL: process.env.REACT_APP_API_URL || 'https://mapcg.onrender.com/', // Usa .env ou fallback
   timeout: 10000, // Tempo limite de 10 segundos
 });
 
